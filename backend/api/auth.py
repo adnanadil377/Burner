366598f0cccda43980ecaa389fabe5fcdf9eae4d
adnan <adnanadil377@gmail.com>
2025-12-08 12:36:35 +0300

    downgraded bcrypt and cleanup of auth flow

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -1,42 +1,28 @@
 from http.client import HTTPResponse
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm,OAuth2,OAuth2PasswordBearer
+from fastapi import APIRouter, Depends, status
+from fastapi.security import OAuth2PasswordRequestForm
 
+from dependency import get_current_user
 from schemas.user import UserCreate
-from controller.auth_controller import create_jwt_token, hash_password, verify_pass
+from controller.auth_controller import authenticate_user, register_new_user
 from models.user import User
 from sqlalchemy.orm import Session
 from db.session import get_db
 
 router = APIRouter()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
-
 @router.post("/token")
 async def login(formData: Annotated[OAuth2PasswordRequestForm, Depends()],db:Session=Depends(get_db)):
-    user = db.query(User).filter(User.email==formData.username).all()
-    if not user:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    if not verify_pass(user.password, formData.password):
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    token = create_jwt_token({"user":user},30)
-    print(token)
-    return {"token":"token"}
+    token=authenticate_user(formData.username, formData.password, db)
+    return {"access_token": token, "token_type": "bearer"}
 
-@router.post("/signup")
+@router.post("/signup",status_code=status.HTTP_201_CREATED)
 async def signup(user_create:UserCreate, db:Session=Depends(get_db)):
-    user = db.query(User).filter(User.email==user_create.email).all()
-    if user:
-        raise HTTPException(status_code=403, detail="Acc already exsits")
-    print("ajsdf",user_create.password)
-    new_user=User(name=user_create.name, email=user_create.email, password=hash_password(user_create.password))
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return {"create":"success"}
+    register_new_user(user_create,db)
+    return {"message": "User created successfully"}
 
 
-@router.post("/items")
-async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {"token": token}
+@router.post("/items",dependencies=[Depends(get_current_user)])
+async def read_items():
+    return {"token": "items"}
