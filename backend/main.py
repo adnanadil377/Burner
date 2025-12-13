c1e4fbfff131254710f1055a4e4aed2b7c0d724d
adnan <adnanadil377@gmail.com>
2025-12-13 09:17:08 +0300

    merged with main branch

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,23 @@ from fastapi import FastAPI
 from api import auth
 from api import video_upload
 from db import base, session
+from fastapi.middleware.cors import CORSMiddleware
 
 base.Base.metadata.create_all(bind=session.engine)
 
 app = FastAPI(title="Burner")
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(video_upload.router, prefix="/upload", tags=["upload"])
