59a9a8a70eac0f2606ac2307f8cc2283866de33f
adnan <adnanadil377@gmail.com>
2025-12-13 09:12:46 +0300

    changes to the cors policy

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 from api import auth
 from db import base, session
+from fastapi.middleware.cors import CORSMiddleware
 
 base.Base.metadata.create_all(bind=session.engine)
 
 app = FastAPI(title="Burner")
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
