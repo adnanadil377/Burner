231766394dedd0f40e9d814d30ad12b71a0e0fb2
adnan <adnanadil377@gmail.com>
2025-12-23 13:39:33 +0300

    removed unnecessary comments

--- backend/controller/auth_controller.py
+++ backend/controller/auth_controller.py
@@ -76,17 +76,3 @@ def create_new_user(create_user: UserCreate,db: Session):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
             detail="Signup failed"
         )
-
-
-# def register_new_user(user_data: UserCreate,db: Session):
-#     user = db.query(User).filter(User.email==user_data.email).first()
-#     if user:
-#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
-
-#     hashed_pwd = get_hash_password(user_data.password)
-#     new_user_email = user_data.email.lower()
-#     new_user = User(name=user_data.name, email=new_user_email, password=hashed_pwd)
-#     db.add(new_user)
-#     db.commit()
-#     db.refresh(new_user)
-#     return new_user
